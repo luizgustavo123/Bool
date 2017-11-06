efa1c92cf1dc46d57e153118c0acfb3a136b83ea
Alunos <Alunos@192.168.12.182>
2017-11-06 21:11:38 -0200

    projeto finalizado

---
+++ Bool/src/bool/boolclass.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bool;
+
+/**
+ *
+ * @author Alunos
+ */
+public class boolclass {
+    
+    
+    
+    public void idade(){
+        int idade = 16;
+        boolean MaiorIdade = 18<=idade;
+        
+        if(MaiorIdade == false){
+        System.out.println("Você é MENOR de idade!!!");
+        }
+        else {
+            System.out.println("Você é MAIOR de idade!!!");
+        }
+        }
+    }
+
+
+        
+        
+    
+    
+    
+    
+
